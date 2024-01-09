17d11ff4293d7520ada7ff53e8bf64d2fc9bf630
amin <biglarymohamadamin75@gmail.com>
2024-01-09 15:08:05 +0330

    add WeatherEntry model

--- core/get_data_app/models.py
+++ core/get_data_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class WeatherEntry(models.Model):
+    layer_name = models.CharField(max_length=300, verbose_name="Layer Name")
+    value = models.FloatField()
+    location = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f"{self.location} - {self.layer_name} - {self.value}"
