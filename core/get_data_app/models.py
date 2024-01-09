21c55d54604fcb990409726c4a77cbbdf79609a4
amin <biglarymohamadamin75@gmail.com>
2024-01-09 17:51:49 +0330

    change models.py for saving data

--- core/get_data_app/models.py
+++ core/get_data_app/models.py
@@ -3,7 +3,9 @@ from django.db import models
 class WeatherEntry(models.Model):
     layer_name = models.CharField(max_length=300, verbose_name="Layer Name")
     value = models.FloatField()
-    location = models.CharField(max_length=300)
+    latitude = models.FloatField(verbose_name="Latitude")
+    longitude = models.FloatField(verbose_name="Longitude")
+    date = models.DateTimeField(verbose_name="Date Recorded", auto_now_add=True)
 
     def __str__(self):
-        return f"{self.location} - {self.layer_name} - {self.value}"
+        return f"{self.latitude}, {self.longitude} - {self.layer_name} - {self.value}"
