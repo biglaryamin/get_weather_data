5f24ffce7230c9fd11226116eba86bfc700ff12b
amin <biglarymohamadamin75@gmail.com>
2024-01-16 21:17:17 +0330

    add save_to_excel function in django admin-panel

--- core/get_data_app/admin.py
+++ core/get_data_app/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from .models import WeatherEntry
 
-admin.site.register(WeatherEntry)
+class WeatherEntryAdmin(admin.ModelAdmin):
+    # Your existing configurations for the admin class
+
+    def save_to_excel(self, request, queryset):
+        # Your custom logic goes here
+        # `queryset` contains the selected objects
+        print("************************")
+        # self.message_user(request, f'Custom function executed for {queryset.count()} items.')
+
+    save_to_excel.short_description = "save_to_excel"
+
+    actions = ['save_to_excel']
+# Register the admin class with the model
+admin.site.register(WeatherEntry, WeatherEntryAdmin)
+
